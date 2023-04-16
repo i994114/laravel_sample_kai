d7e4b7b422c735628d859f7b323c2444f7f8a247
i994114 <i994114@hotmail.com>
2023-04-16 14:07:59 +0900

    ログインしないと機能を使えないように実装

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,13 @@ Route::get('contact', 'ContactController@index');
 Route::post('contact', 'ContactController@send');
 
 //drill
-Route::get('/drills/new', 'DrillsController@new')->name(('drills.new'));
-Route::post('/drills/new', 'DrillsController@create');
-Route::get('/drills', 'DrillsController@index')->name('drills');
-Route::get('/drills/{id}/edit', 'DrillsController@edit')->name('drills.edit');
-Route::post('/drills/{id}', 'DrillsController@update')->name('drills.update');
-Route::post('/drills/{id}/delete', 'DrillsController@destroy')->name('drills.delete');
-Route::get('/drills/{id}', 'DrillsController@show')->name('drills.show');
-Route::get('/mypage', 'DrillsController@mypage')->name('drills.mypage');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/drills/new', 'DrillsController@new')->name(('drills.new'));
+    Route::post('/drills/new', 'DrillsController@create');
+    Route::get('/drills', 'DrillsController@index')->name('drills');
+    Route::get('/drills/{id}/edit', 'DrillsController@edit')->name('drills.edit');
+    Route::post('/drills/{id}', 'DrillsController@update')->name('drills.update');
+    Route::post('/drills/{id}/delete', 'DrillsController@destroy')->name('drills.delete');
+    Route::get('/drills/{id}', 'DrillsController@show')->name('drills.show');
+    Route::get('/mypage', 'DrillsController@mypage')->name('drills.mypage');
+});
